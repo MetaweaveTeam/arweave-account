141245ea2173ba60c7097f5f6c394c05888ce4ee
bidetaggle <bidetaggle@gmail.com>
2022-03-05 15:26:44 +0000

    solve errors related to ArDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import Arweave from 'arweave';
+import ArDB from 'ardb';
+
 export default class Account {
+  private arweave: Arweave;
+  private ardb: ArDB;
+  
   constructor() {
-    
+    this.arweave = Arweave.init({
+      host: 'arweave.net',// Hostname or IP address for a Arweave host
+      port: 443,          // Port
+      protocol: 'https',  // Network protocol http or https
+      timeout: 20000,     // Network request timeouts in milliseconds
+      logging: false,
+    });
+    this.ardb = new ArDB(this.arweave);
   }
 
-  getSomething(): string {
+  getSomething(jwk: string): string {
     return "hello from arweave-account";
   }
 }
