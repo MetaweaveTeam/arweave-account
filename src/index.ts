d2f4fe8ecaa66017c0153e6f4dc9286f5b3e2fcd
Tom Wilson <tom@hyper.io>
2022-05-12 12:43:32 -0400

    chg: replaced `transactions.getData` with `api.get` (#2)
    * changed getData to api.get, added dependencies ardb arweave and added uvu nock and tsm for tests
    
    * fix formating with prettier
    
    * fix typescript errors
    
    * changed tsconfig for es2015 target and all ts warnings went away and was able to build the lib
    
    Co-authored-by: bidetaggle <bidetaggle@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,12 @@ export default class Account {
     cacheSize = 100,
     cacheTime = 60000,
     gateway = {
-      host: 'arweave.net',// Hostname or IP address for a Arweave host
-      port: 443,          // Port
-      protocol: 'https',  // Network protocol http or https
-      timeout: 20000,     // Network request timeouts in milliseconds
+      host: 'arweave.net', // Hostname or IP address for a Arweave host
+      port: 443, // Port
+      protocol: 'https', // Network protocol http or https
+      timeout: 20000, // Network request timeouts in milliseconds
       logging: false,
-    }
+    },
   } = {}) {
     this.arweave = Arweave.init(gateway);
     this.ardb = new ArDB(this.arweave);
@@ -30,34 +30,32 @@ export default class Account {
   async get(addr: T_addr): Promise<T_account | null> {
     addr = addr.trim();
     let cacheResponse;
-    if(cacheResponse = this.cache.get(addr))
-      return cacheResponse;
+    if ((cacheResponse = this.cache.get(addr))) return cacheResponse;
     else {
-      const tx: transaction[] | block[] = await this.ardb.search('transactions')
-      .tag('Protocol-Name', 'Account-0.2')
-      .from(addr)
-      .limit(1).find();
-      
-      const data = tx[0]?.id 
-      ? await this.arweave.transactions.getData(tx[0].id, { decode: true, string: true })
-      : null;
-      
-      if(typeof data === "string"){
+      const tx: transaction[] | block[] = await this.ardb
+        .search('transactions')
+        .tag('Protocol-Name', 'Account-0.2')
+        .from(addr)
+        .limit(1)
+        .find();
+
+      const data = tx[0]?.id ? await this.arweave.transactions.getData(tx[0].id, { decode: true, string: true }) : null;
+
+      if (typeof data === 'string') {
         let profile = JSON.parse(data);
         profile = {
           ...profile,
-          handle: `${profile.handle}#${addr.slice(0,3)}${addr.slice(addr.length-3)}`,
-          addr: addr
-        }
+          handle: `${profile.handle}#${addr.slice(0, 3)}${addr.slice(addr.length - 3)}`,
+          addr: addr,
+        };
         const account = {
           txid: tx[0].id,
-          profile
+          profile,
         };
 
         this.cache.hydrate(addr, account);
         return account;
-      }
-      else{
+      } else {
         this.cache.hydrate(addr);
         return null;
       }
@@ -65,86 +63,83 @@ export default class Account {
   }
 
   async search(handle: string): Promise<T_account[]> {
-    const txs: transaction[] | block[] = await this.ardb.search('transactions')
-    .tag('Protocol-Name', 'Account-0.2')
-    .tag('handle', handle)
-    .limit(100).find();
+    const txs: transaction[] | block[] = await this.ardb
+      .search('transactions')
+      .tag('Protocol-Name', 'Account-0.2')
+      .tag('handle', handle)
+      .limit(100)
+      .find();
 
-    const formattedAccounts = txs.map(async tx => {
+    const formattedAccounts = txs.map(async (tx) => {
       const txid: T_txid = tx.id;
-      const data = await this.arweave.transactions.getData(txid, { decode: true, string: true });
+      let profile = (
+        await this.arweave.api.get(txid).catch(() => {
+          return { data: null };
+        })
+      ).data;
       const addr = 'owner' in tx ? tx.owner.address : 'anonymous';
-      if(typeof data === "string"){
-        let profile: T_profile = JSON.parse(data);
-        profile = {
-          ...profile,
-          handle: `${profile.handle}#${addr.slice(0,3)}${addr.slice(addr.length-3)}`,
-          addr: addr
-        }
-        return {
-          txid: txid,
-          profile
-        } as T_account
-      }
-      else return null
+      profile = {
+        ...profile,
+        addr,
+        handle: `${profile.handle}#${addr.slice(0, 3)}${addr.slice(addr.length - 3)}`,
+      };
+      return {
+        txid: txid,
+        profile,
+      } as T_account;
     });
 
     const accounts = await Promise.all(formattedAccounts);
 
     return accounts.filter(
-      (v,i,a): v is T_account => 
-      v !== null &&
-      // remove address duplicates: https://stackoverflow.com/a/56757215
-      a.findIndex(t => (t?.profile.addr===v?.profile.addr)) === i
+      (v, i, a): v is T_account =>
+        v !== null &&
+        // remove address duplicates: https://stackoverflow.com/a/56757215
+        a.findIndex((t) => t?.profile.addr === v?.profile.addr) === i,
     );
   }
-  
+
   async find(uniqueHandle: string): Promise<T_account | null> {
     let cacheResponse;
     uniqueHandle = uniqueHandle.trim();
     // check if format is handle#xxxxxx
-    if(!/^(.+)#[a-zA-Z0-9\-\_]{6}$/.test(uniqueHandle))
-      return null;
-    
-    if(cacheResponse = this.cache.find(uniqueHandle))
-      return cacheResponse;
-    else {
-      const txs: transaction[] | block[] = await this.ardb.search('transactions')
-      .tag('Protocol-Name', 'Account-0.2')
-      .tag('handle', uniqueHandle.slice(0,-7))
-      .limit(100).find();
+    if (!/^(.+)#[a-zA-Z0-9\-\_]{6}$/.test(uniqueHandle)) return null;
 
-      const formattedAccounts = txs.map(async tx => {
+    if ((cacheResponse = this.cache.find(uniqueHandle))) return cacheResponse;
+    else {
+      const txs: transaction[] | block[] = await this.ardb
+        .search('transactions')
+        .tag('Protocol-Name', 'Account-0.2')
+        .tag('handle', uniqueHandle.slice(0, -7))
+        .limit(100)
+        .find();
+
+      const formattedAccounts = txs.map(async (tx) => {
         const txid: T_txid = tx.id;
-        const data = await this.arweave.transactions.getData(txid, { decode: true, string: true });
+        let profile = (
+          await this.arweave.api.get(txid).catch(() => {
+            return { data: null };
+          })
+        ).data;
         const addr = 'owner' in tx ? tx.owner.address : 'anonymous';
-        if(typeof data === "string"){
-          let profile: T_profile = JSON.parse(data);
-          if(profile.handle+'#'+addr.slice(0,3)+addr.slice(addr.length-3) === uniqueHandle){
-            profile = {
-              ...profile,
-              handle: `${profile.handle}#${addr.slice(0,3)}${addr.slice(addr.length-3)}`,
-              addr: addr
-            }
-            return {
-              txid: txid,
-              profile
-            }
-          }
-          else return null;
-        }
-        else return null;
+        profile = {
+          ...profile,
+          addr,
+          handle: `${profile.handle}#${addr.slice(0, 3)}${addr.slice(addr.length - 3)}`,
+        };
+        return {
+          txid: txid,
+          profile,
+        } as T_account;
       });
 
       const a = await Promise.all(formattedAccounts);
       const accounts = a.filter((e): e is T_account => e !== null);
 
-      if(accounts.length > 0){
+      if (accounts.length > 0) {
         this.cache.hydrate(accounts[0].profile.addr, accounts[0]);
         return accounts[0];
-      }
-      else
-        return null;
+      } else return null;
     }
   }
 
--- src/types.ts
+++ src/types.ts
@@ -2,28 +2,28 @@ type T_addr = string;
 type T_txid = string;
 
 type T_profile = {
-  addr: T_addr,
-  handle: string,
-  name: string,
-  bio: string,
+  addr: T_addr;
+  handle: string;
+  name: string;
+  bio: string;
   links: {
-    twitter?: string,
-    instagram?: string,
-    github?: string,
-    facebook?: string
-  },
-  avatar: T_txid
-}
+    twitter?: string;
+    instagram?: string;
+    github?: string;
+    facebook?: string;
+  };
+  avatar: T_txid;
+};
 
 type T_account = {
-  txid: T_txid,
-  profile: T_profile
-}
+  txid: T_txid;
+  profile: T_profile;
+};
 
 type T_item = {
-  timestamp: number,
-  addr: T_addr,
-  account: T_account | null
-}
+  timestamp: number;
+  addr: T_addr;
+  account: T_account | null;
+};
 
-export type {T_addr, T_txid, T_profile, T_account, T_item}
+export type { T_addr, T_txid, T_profile, T_account, T_item };
